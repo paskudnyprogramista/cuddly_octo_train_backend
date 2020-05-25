d28d405f1d94bb838f941488079d3f63f8309083
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-25 23:48:22 +0200

    Add has_many association for LineItem in Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@
 class Product < ApplicationRecord
   validates :sku, :name, :price, presence: true
   validates :price, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
+
+  has_many :line_items, -> { order(:position) }, inverse_of: :product, class_name: 'LineItem'
 end
