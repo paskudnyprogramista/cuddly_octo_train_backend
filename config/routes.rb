3c11a3e21d36ef3f751abfe0c0b08891ead34e6b
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-25 23:48:43 +0200

    Add routes for Orders and Cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products, only: %i[index show]
+      resources :orders, only: :show
+
+      resource :cart, controller: :cart, only: :show do
+        post :add_item
+        delete 'remove_item/:line_item_id', to: 'cart#remove_item', as: :remove_item
+      end
     end
   end
 end
