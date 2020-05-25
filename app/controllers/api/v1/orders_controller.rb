0bf046c0fe3bbcbf6e4c36834a0b2f4d515e810b
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-25 23:44:34 +0200

    Create Orders API Controller

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class OrdersController < ApplicationController
+      def show
+        # TODO: Authorize user
+        # TODO: Impl method responsible for showing order data
+      end
+    end
+  end
+end
