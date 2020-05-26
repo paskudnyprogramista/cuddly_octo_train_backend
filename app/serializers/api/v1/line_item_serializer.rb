af8f4bea5f7fa7d9649482109654835ab8497936
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-26 22:51:34 +0200

    Add product details in line items serializer

--- app/serializers/api/v1/line_item_serializer.rb
+++ app/serializers/api/v1/line_item_serializer.rb
@@ -5,6 +5,14 @@ module Api
     class LineItemSerializer < BaseSerializer
       attributes :quantity, :price
 
+      attribute :product_name do |resource|
+        resource.product.name
+      end
+
+      attribute :product_id do |resource|
+        resource.product.id
+      end
+
       belongs_to :order, links: {
         self: lambda { |resource|
           url_helpers.api_v1_order_url(resource.order_id)
