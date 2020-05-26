72d271b1c2ffc5674979face45ebd694990f78bb
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-26 14:34:06 +0200

    Set price and add price validation to LineItem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,14 @@
 class LineItem < ApplicationRecord
   belongs_to :order, touch: true, class_name: 'Order', inverse_of: :line_items
   belongs_to :product, class_name: 'Product', inverse_of: :line_items
+
+  validates :price, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
+
+  before_validation :copy_price
+
+  private
+
+  def copy_price
+    self.price = product.price
+  end
 end
