60a03da4700975941edd71bd899f69a5dc421b56
paskudnyprogramista <paskudnyprogramista@pm.me>
2020-05-26 14:34:33 +0200

    Implement total calculations in RecalculateCart service

--- app/services/cart/recalculate_cart.rb
+++ app/services/cart/recalculate_cart.rb
@@ -3,11 +3,18 @@
 module Cart
   class RecalculateCart < ::BaseService
     class << self
+      # @param order Order
       def call(order:)
+        item_total = order.line_items.sum('price * quantity')
+        # TODO: Impl Promotions
+        # Promotions will have Rules, which will be used to calculate adjustments
+        adjustment_total = 0.00
+        total = item_total + adjustment_total
+
         order.update_columns(
-          total: 0.00,
-          item_total: 0.00,
-          adjustment_total: 0.00,
+          total: total,
+          item_total: item_total,
+          adjustment_total: adjustment_total,
           updated_at: Time.current
         )
 
